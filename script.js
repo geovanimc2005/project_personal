3c2b91eda4618f04ff03cb8a9060b42278cb43e6
Geovani M Coratti <87933178+geovanimc2005@users.noreply.github.com>
2025-06-24 02:35:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+//programado por geovani moura coratti: 22/06/2025
+
 const tabButtonsContainer = document.getElementById('tabButtons');
 const tabContents = document.querySelectorAll('.tab-content');
 const button1 = document.getElementById("data-btn");
 const error = document.getElementById("error");
 const conteudo = document.getElementById("principal");
+const button = document.getElementById("botao");
+const button2 = document.getElementById("area");
+var geo = document.getElementById("area");
 tabButtonsContainer.addEventListener('click', (event) => {
     const clickedButton = event.target;
     const targetContentId = clickedButton.dataset.tab;
@@ -37,8 +42,46 @@ async function data(){
     }catch(erro){
         console.log(erro);
         error.textContent = erro;
+        error.blur();
     }
 }
+async function apenas_soluções(){
+    const resposta = document.getElementById("only");
+    var asa = 0;
+    var variaveis = [];
+    for(i = 0; i < 10; i++){
+        asa += i * 2;
+        variaveis.push(asa);
+    }
+    resposta.textContent = variaveis;
+}
+async function salvar(){
+    var conteud = conteudo.value;
+    const palavras = ["foda", "caralho"];
+    const a = document.createElement('li');
+    const blob = new Blob([conteud], {type: "text/plain" })
+    const url = URL.createObjectURL(blob);
+    a.href = url;
+    a.download = "asa2asa";
+    document.body.appendChild(a);
+    for(i = 0; i < palavras.length; i++){
+        if(palavras[i] == geo.value){
+            alert("apague este palavrão");
+        }
+    }
+    a.click();
+    setTimeout(() => {
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+        console.log(`Arquivo "asasas" gerado e download iniciado.`);
+    }, 100);
+}
+
+const square = t => Math.sign(Math.sin(t));
 button1.addEventListener('click', () =>{
     data();
+
+});
+button.addEventListener("click", () =>{
+    salvar();
 });
